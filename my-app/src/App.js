1f72a00b6388bd0c6e6e8dbbf0f459f5785d04d5
Василь Ярош <v.yarosh@bgv.com.ua>
2024-08-02 18:43:34 +0300

    added new text

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,8 +4,25 @@ import InputForm from "./components/InputForm"
 function App() {
   return (
     <div className="App">
-      <h1 style={{textAlign: 'center'}}>Створіть ваш власний email підпис</h1>
-      <InputForm/>
+     
+      <div className='input-container'>
+       <InputForm />  
+      </div>
+      
+       {/* <div className='arrow-container'>
+        <div class="arrow arrow-first"></div>
+        <div class="arrow arrow-second"></div>
+      </div>
+     
+      <div className='video-container'>
+        <div>
+          <iframe src="https://bgvgm-my.sharepoint.com/personal/v_yarosh_bgv_com_ua/_layouts/15/embed.aspx?UniqueId=d33df550-a809-452b-884b-3704bf6d8f28&embed=%7B%22ust%22%3Atrue%2C%22hv%22%3A%22CopyEmbedCode%22%7D&referrer=StreamWebApp&referrerScenario=EmbedDialog.Create" width="1280" height="720" frameborder="0" scrolling="yes" allowfullscreen title="Інструкція підпис WEB"></iframe>
+        </div>
+        <div>
+          <iframe src="https://bgvgm-my.sharepoint.com/personal/v_yarosh_bgv_com_ua/_layouts/15/embed.aspx?UniqueId=5d580c68-37ae-4c53-a3f9-d99ec25d4bbe&embed=%7B%22ust%22%3Atrue%2C%22hv%22%3A%22CopyEmbedCode%22%7D&referrer=StreamWebApp&referrerScenario=EmbedDialog.Create" width="1280" height="720" frameborder="0" scrolling="yes" allowfullscreen title="Інструкція підпис PC"></iframe>
+        </div>
+      </div> */}
+
     </div>
   );
 }
--- my-app/src/components/InputForm.js
+++ my-app/src/components/InputForm.js
@@ -4,11 +4,14 @@ import GeneratedSignature from "./GeneratedSignature"
 function InputForm() {
     return (
         <div className="App">
+             <h1 style={{ textAlign: 'center' }}>Створіть ваш власний email підпис</h1>
             <h2 style={{textAlign: 'center'}}>Введіть ваші дані для створення підпису</h2>
+            
             <div className="gap-container">
                 <div >
                     <GeneratedSignature />
                 </div> 
+                
             </div>
         </div>
     );
